992399ea96256f53f73489d97cbe66a1ffd16854
REZ <147749303+letrezdraw@users.noreply.github.com>
2025-09-18 10:56:20 +0000

    gf

--- Ass-4/C2.c
+++ Ass-4/C2.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 int main(){
-    int ref[]={2,5,2,8,5,4,1,2,3,2,6,1,2,5,9,8},n,i,j,k,faults=0,len=16,frames[20],count=0;
+    int ref[]={2,5,2,8,5,4,1,2,3,2,6,1,2,5,9,8},n,i,j,k,f=0,l=16,fm[20],c=0;
     printf("Enter number of memory frames: "); scanf("%d",&n);
-    for(i=0;i<len;i++){
+    for(i=0;i<l;i++){
         printf("Page %d: ",ref[i]);
-        for(j=0;j<count && frames[j]!=ref[i];j++);
-        if(j<count){
+        for(j=0;j<c && fm[j]!=ref[i];j++);
+        if(j<c){
             printf("Hit [");
-            for(k=0;k<count;k++) printf("%d%s",frames[k],k<count-1?", ":"");
+            for(k=0;k<c;k++) printf("%d%s",fm[k],k<c-1?", ":"");
             printf("]\n");
-            for(;j>0;j--) frames[j]=frames[j-1];
-            frames[0]=ref[i];
-        } else {
-            faults++;
-            if(count<n) {
-                for(j=count;j>0;j--) frames[j]=frames[j-1];
-                frames[0]=ref[i];
-                count++;
-            } else {
-                for(j=n-1;j>0;j--) frames[j]=frames[j-1];
-                frames[0]=ref[i];
+            for(;j>0;j--) fm[j]=fm[j-1];
+            fm[0]=ref[i];
+        }else{
+            f++;
+            if(c<n){
+                for(j=c++;j>0;j--) fm[j]=fm[j-1];
+                fm[0]=ref[i];
+            }else{
+                for(j=n-1;j>0;j--) fm[j]=fm[j-1];
+                fm[0]=ref[i];
             }
             printf("Miss [");
-            for(k=0;k<count;k++) printf("%d%s",frames[k],k<count-1?", ":"");
+            for(k=0;k<c;k++) printf("%d%s",fm[k],k<c-1?", ":"");
             printf("]\n");
         }
     }
-    printf("Total Page Faults: %d\n",faults);
+    printf("Total Page Faults: %d\n",f);
     return 0;
 }
